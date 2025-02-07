a9fce43c0358935aa20863ea201842ae5c17845c
nabee-lhassan <your.nabeelhassa02@gmail.com>
2025-02-07 08:24:11 +0500

    change

--- index.php
+++ index.php
@@ -16,18 +16,18 @@ function add_custom_fields_to_product_page() {
             <input type="text" id="custom_message" name="custom_message" />
           </div>';
     
+    // Team Name Field
     echo '<div class="custom-field">
             <label for="team_name">Team Name</label>
             <input type="text" id="team_name" name="team_name" />
           </div>';
     
+    // Player Information Section
     echo '<fieldset class="custom-field" style="border: 1px solid #ddd; padding: 10px; margin-bottom: 10px;">
             <legend><strong>Player Information</strong></legend>';
     
-    echo '<div class="player-group" style="display: flex; gap: 10px;">';
-    
-    // Size Field
-    echo '<div class="custom-field">
+    // Player Group Fields
+    echo '<div class="player-group">
             <label for="player_size">Size</label>
             <select id="player_size" name="player_size">
                 <option value="small">Small</option>
@@ -37,27 +37,25 @@ function add_custom_fields_to_product_page() {
             </select>
           </div>';
     
-    // Player Name Field
-    echo '<div class="custom-field">
+    echo '<div class="player-group">
             <label for="player_name">Player Name</label>
             <input type="text" id="player_name" name="player_name" />
           </div>';
-    
-    // Player Number Field
-    echo '<div class="custom-field">
+
+    echo '<div class="player-group">
             <label for="player_number">Player Number</label>
             <input type="text" id="player_number" name="player_number" />
           </div>';
     
-    echo '</div>'; // End of Player Group
-    
     echo '</fieldset>'; // End of Player Information Section
 
+    // Logo Upload Field
     echo '<div class="custom-field">
             <label for="logo_upload">Logo (Upload)</label>
             <input type="file" id="logo_upload" name="logo_upload" />
           </div>';
     
+    // Sponsor Option Field
     echo '<div class="custom-field">
             <label for="sponsor_option">Sponsor Option</label>
             <select id="sponsor_option" name="sponsor_option">
@@ -85,11 +83,43 @@ function save_custom_fields_to_cart( $cart_item_data, $product_id ) {
     if( isset( $_POST['player_number'] ) ) {
         $cart_item_data['player_number'] = sanitize_text_field( $_POST['player_number'] );
     }
-    if( isset( $_FILES['logo_upload'] ) ) {
-        $cart_item_data['logo_upload'] = $_FILES['logo_upload'];
-    }
-    if( isset( $_POST['sponsor_option'] ) ) {
-        $cart_item_data['sponsor_option'] = sanitize_text_field( $_POST['sponsor_option'] );
-    }
     return $cart_item_data;
+}
+
+// Display custom fields in the cart
+add_filter( 'woocommerce_get_item_data', 'display_custom_fields_in_cart', 10, 2 );
+function display_custom_fields_in_cart( $item_data, $cart_item ) {
+    if( isset( $cart_item['player_size'] ) ) {
+        $item_data[] = array(
+            'name' => 'Player Size',
+            'value' => ucfirst($cart_item['player_size'])
+        );
+    }
+    if( isset( $cart_item['player_name'] ) ) {
+        $item_data[] = array(
+            'name' => 'Player Name',
+            'value' => $cart_item['player_name']
+        );
+    }
+    if( isset( $cart_item['player_number'] ) ) {
+        $item_data[] = array(
+            'name' => 'Player Number',
+            'value' => $cart_item['player_number']
+        );
+    }
+    return $item_data;
+}
+
+// Display custom field values in the checkout
+add_action( 'woocommerce_checkout_create_order_line_item', 'add_custom_fields_to_order', 10, 4 );
+function add_custom_fields_to_order( $item, $cart_item_key, $values, $order ) {
+    if( isset( $values['player_size'] ) ) {
+        $item->add_meta_data( 'Player Size', ucfirst($values['player_size']) );
+    }
+    if( isset( $values['player_name'] ) ) {
+        $item->add_meta_data( 'Player Name', $values['player_name'] );
+    }
+    if( isset( $values['player_number'] ) ) {
+        $item->add_meta_data( 'Player Number', $values['player_number'] );
+    }
 }
