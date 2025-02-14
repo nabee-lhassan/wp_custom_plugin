615983c25f41ceddd5b7928395440c5e8d688879
nabee-lhassan <your.nabeelhassa02@gmail.com>
2025-02-14 14:44:19 +0500

    change 45

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
  * Text Domain: woocommerce-custom-fields
  */
 
-// Display custom fields on product page (for specific category)
+// Display custom fields on product page
 add_action('woocommerce_before_add_to_cart_button', 'add_custom_fields_to_product_page');
 function add_custom_fields_to_product_page() {
     global $post;
@@ -19,7 +19,7 @@ function add_custom_fields_to_product_page() {
     echo '<div class="custom-fields">
             <label for="customer_name">Customer Name:</label>
             <input type="text" id="customer_name" name="custom_customer_name" required />
-            
+
             <label for="phone_number">Phone Number:</label>
             <input type="tel" id="phone_number" name="custom_phone_number" required />
 
@@ -31,9 +31,20 @@ function add_custom_fields_to_product_page() {
             <img id="brand_logo_preview" style="max-width:100px; display:none;" />
           </div>
 
-          <button type="button" id="custom_add_button" class="button alt">Custom Add to Cart</button>
-          
+          <!-- Elementor Popup Button -->
+          <button type="button" id="elementor_popup_button" class="button alt" onclick="openElementorPopup()">Open Popup</button>
+
           <script>
+          function openElementorPopup() {
+              // Elementor Popup Trigger
+              jQuery.magnificPopup.open({
+                  items: {
+                      src: "#elementor-popup-id",
+                      type: "inline"
+                  }
+              });
+          }
+
           document.addEventListener("DOMContentLoaded", function() {
               document.getElementById("brand_logo").addEventListener("change", function(event) {
                   var reader = new FileReader();
@@ -43,74 +54,75 @@ function add_custom_fields_to_product_page() {
                   };
                   reader.readAsDataURL(event.target.files[0]);
               });
-
-              // Custom button trigger for add to cart
-              document.getElementById("custom_add_button").addEventListener("click", function() {
-                  document.querySelector("form.cart").submit();
-              });
           });
           </script>';
 }
 
-// Save custom fields to cart
+// Save custom fields in cart
 add_filter('woocommerce_add_cart_item_data', 'save_custom_fields_to_cart', 10, 2);
 function save_custom_fields_to_cart($cart_item_data, $product_id) {
-    if (!empty($_POST['custom_customer_name'])) {
+    if (isset($_POST['custom_customer_name'])) {
         $cart_item_data['custom_customer_name'] = sanitize_text_field($_POST['custom_customer_name']);
     }
-    if (!empty($_POST['custom_phone_number'])) {
+    if (isset($_POST['custom_phone_number'])) {
         $cart_item_data['custom_phone_number'] = sanitize_text_field($_POST['custom_phone_number']);
     }
-    if (!empty($_POST['custom_order_notes'])) {
+    if (isset($_POST['custom_order_notes'])) {
         $cart_item_data['custom_order_notes'] = sanitize_textarea_field($_POST['custom_order_notes']);
     }
     if (!empty($_FILES['custom_brand_logo']['name'])) {
-        $upload = wp_handle_upload($_FILES['custom_brand_logo'], ['test_form' => false]);
-
-        if (!isset($upload['error']) && isset($upload['url'])) {
-            $cart_item_data['custom_brand_logo'] = esc_url($upload['url']);
-            WC()->session->set('custom_brand_logo', esc_url($upload['url'])); 
-        } else {
-            wc_add_notice(__('Brand Logo upload failed. Please try again.', 'woocommerce'), 'error');
+        $upload = wp_upload_bits($_FILES['custom_brand_logo']['name'], null, file_get_contents($_FILES['custom_brand_logo']['tmp_name']));
+        if (!$upload['error']) {
+            $cart_item_data['custom_brand_logo'] = $upload['url'];
         }
     }
     return $cart_item_data;
 }
 
-// Display custom fields in the cart
+// Display custom fields in cart & checkout
 add_filter('woocommerce_get_item_data', 'display_custom_fields_in_cart', 10, 2);
 function display_custom_fields_in_cart($item_data, $cart_item) {
-    if (!empty($cart_item['custom_customer_name'])) {
-        $item_data[] = ['name' => 'Customer Name', 'value' => esc_html($cart_item['custom_customer_name'])];
-    }
-    if (!empty($cart_item['custom_phone_number'])) {
-        $item_data[] = ['name' => 'Phone Number', 'value' => esc_html($cart_item['custom_phone_number'])];
-    }
-    if (!empty($cart_item['custom_order_notes'])) {
-        $item_data[] = ['name' => 'Order Notes', 'value' => esc_html($cart_item['custom_order_notes'])];
-    }
-    if (!empty($cart_item['custom_brand_logo'])) {
+    if (isset($cart_item['custom_customer_name'])) {
         $item_data[] = [
-            'name'  => 'Brand Logo',
-            'value' => '<img src="' . esc_url($cart_item['custom_brand_logo']) . '" style="max-width:50px;" />'
+            'name' => 'Customer Name',
+            'value' => sanitize_text_field($cart_item['custom_customer_name'])
+        ];
+    }
+    if (isset($cart_item['custom_phone_number'])) {
+        $item_data[] = [
+            'name' => 'Phone Number',
+            'value' => sanitize_text_field($cart_item['custom_phone_number'])
+        ];
+    }
+    if (isset($cart_item['custom_order_notes'])) {
+        $item_data[] = [
+            'name' => 'Order Notes',
+            'value' => sanitize_textarea_field($cart_item['custom_order_notes'])
+        ];
+    }
+    if (isset($cart_item['custom_brand_logo'])) {
+        $item_data[] = [
+            'name' => 'Brand Logo',
+            'value' => '<img src="'.esc_url($cart_item['custom_brand_logo']).'" style="max-width:100px;" />'
         ];
     }
     return $item_data;
 }
 
 // Save custom fields in order meta
-add_action('woocommerce_checkout_create_order_line_item', 'add_custom_fields_to_order', 10, 4);
-function add_custom_fields_to_order($item, $cart_item_key, $values, $order) {
-    if (!empty($values['custom_customer_name'])) {
-        $item->add_meta_data('Customer Name', esc_html($values['custom_customer_name']));
+add_action('woocommerce_checkout_create_order_line_item', 'save_custom_fields_to_order_meta', 10, 4);
+function save_custom_fields_to_order_meta($item, $cart_item_key, $values, $order) {
+    if (isset($values['custom_customer_name'])) {
+        $item->add_meta_data('Customer Name', $values['custom_customer_name'], true);
     }
-    if (!empty($values['custom_phone_number'])) {
-        $item->add_meta_data('Phone Number', esc_html($values['custom_phone_number']));
+    if (isset($values['custom_phone_number'])) {
+        $item->add_meta_data('Phone Number', $values['custom_phone_number'], true);
     }
-    if (!empty($values['custom_order_notes'])) {
-        $item->add_meta_data('Order Notes', esc_html($values['custom_order_notes']));
+    if (isset($values['custom_order_notes'])) {
+        $item->add_meta_data('Order Notes', $values['custom_order_notes'], true);
     }
-    if (!empty($values['custom_brand_logo'])) {
-        $item->add_meta_data('Brand Logo', '<img src="' . esc_url($values['custom_brand_logo']) . '" style="max-width:50px;" />');
+    if (isset($values['custom_brand_logo'])) {
+        $item->add_meta_data('Brand Logo', '<img src="'.esc_url($values['custom_brand_logo']).'" style="max-width:100px;" />', true);
     }
 }
+?>
