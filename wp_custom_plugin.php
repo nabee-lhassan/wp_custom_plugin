e9f69dff3d8db7088a06343fba7df2ef534d00db
nabee-lhassan <your.nabeelhassa02@gmail.com>
2025-03-21 15:07:49 +0500

    test change

--- wp_custom_plugin.php
+++ wp_custom_plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce Custom Product Fields
  * Plugin URI: https://example.com/woocommerce-custom-fields
  * Description: Adds custom fields to WooCommerce product pages and captures customer input.
- * Version: 2.5
+ * Version: 2.5.1
  * Author: Nabeel Hassan 
  * Author URI: https://nabeelhassan.com
  * Text Domain: woocommerce-custom-fields
